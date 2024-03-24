fcc91071f5e1138f0708597ed26d371c0af24169
David <daviddenisson2014@gmail.com>
2024-03-23 23:16:15 -0300

    adicionando aside animacao

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ElementRef, AfterViewInit} from '@angular/core';
 
 
 import { ActualpageService } from 'src/app/services/actualpage.service';
@@ -12,7 +12,40 @@ export class NavbarComponent {
 
   currentPage: string = '';
 
-  constructor(private actualPage: ActualpageService){}
+  constructor(private actualPage: ActualpageService, private elementRef: ElementRef){}
+
+  ngAfterViewInit(): void {
+
+    const hamburguerButton = this.elementRef.nativeElement.querySelector('.hamburguer');
+
+    hamburguerButton.addEventListener('click', (e:any) => {
+
+      let aside = this.elementRef.nativeElement.querySelector('aside');
+
+      if(!aside.classList.contains('active')){
+
+        aside.classList.add('active');
+
+      }
+
+    });
+
+    const closeAsideButton = this.elementRef.nativeElement.querySelector('.aside-header .icon');
+
+    closeAsideButton.addEventListener('click', (e:any) => {
+
+      let aside = this.elementRef.nativeElement.querySelector('aside');
+
+
+      if(aside.classList.contains('active')){
+
+        aside.classList.remove('active');
+
+      }
+
+    })
+
+  }
 
   ngOnInit() {
 
@@ -22,7 +55,11 @@ export class NavbarComponent {
 
     })
 
+
+
   }
+
+
 
 
 
