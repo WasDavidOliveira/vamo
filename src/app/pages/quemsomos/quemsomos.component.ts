5748c7cf83a1e9db37098530659294a0d6c8a853
David <daviddenisson2014@gmail.com>
2024-05-14 05:55:21 -0300

    add quem somos e planos arrumado

--- src/app/pages/quemsomos/quemsomos.component.ts
+++ src/app/pages/quemsomos/quemsomos.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActualpageService } from 'src/app/services/actualpage.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'quemsomos-page',
@@ -13,6 +14,24 @@ export class QuemsomosComponent {
     this.pageService.setCurrentPage('quem-somos');
 
   }
+
+  customOptions: OwlOptions = {
+    loop: true,
+    dots: true,
+    items: 1,
+    margin: 0,
+    nav: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    navText: [
+      '<i class="fa-solid fa-chevron-left"></i>',
+      '<i class="fa-solid fa-chevron-right"></i>',
+    ],
+    autoWidth: true,
+    autoHeight: true,
+  };
 
 
 }
