3657ff613c4a6f5616fab85a67b32a205626bdda
David <daviddenisson2014@gmail.com>
2024-05-14 05:18:02 -0300

    add combos com novo card e home com novo plano

--- src/app/pages/planos/planos.component.ts
+++ src/app/pages/planos/planos.component.ts
@@ -19,7 +19,7 @@ export class PlanosComponent {
 
     loop: false,
     items: 3,
-    margin: 10,
+    margin: 0,
     nav: false,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -30,127 +30,107 @@ export class PlanosComponent {
   };
 
   ngAfterViewInit(): void {
-    const choicesApps =
-      this.elementRef.nativeElement.querySelectorAll('.app img');
 
-    if (choicesApps != null) {
-      choicesApps.forEach((app: any) => {
-        app.addEventListener('click', (appClicado: any) => {
-          let qualPlano =
-            appClicado.target.parentNode.parentNode.parentNode.parentNode;
-          let qualApp = appClicado.target.alt;
 
-          let appSelecionado = appClicado.target;
+    const choicesApps = this.elementRef.nativeElement.querySelectorAll('.app-choice');
 
-          let urlPlanoStandardPadrao =
-            'https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20300%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%2024/7,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20HD%20por%20apenas%20R$%2064,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?';
-          let urlPlanoPremiumPadrao =
-            'https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20500%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%2024/7,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20HD%20por%20apenas%20R$%2084,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?';
+    choicesApps.forEach((choiceApp: any) => {
 
-          let urlPlanoStandardAddApp =
-            'https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20300%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%2024/7,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20HD%20%2B%201%20app%20standard%20por%20apenas%20R%24%2064,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?';
-          let urlPlanoPremiumAddApp =
-            'https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20500%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%2024/7,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20HD%20%2B%201%20app%20premium%20por%20apenas%20R%24%2084,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?';
+      choiceApp.addEventListener('click', (element: any) => {
 
-          if (qualPlano.classList.contains('plano-standard')) {
-            let newText = '20%2B%20o%20aplicativo%20' + qualApp + '%';
+        let div = element.target;
 
-            let cardPlano = document.querySelector(
-              '.plano.plano-standard .card-plano'
-            );
+        if (!div.classList.contains('app-choice')) {
 
-            let resetCardPlano = document.querySelector(
-              '.plano.plano-premium .card-plano'
-            );
+          div = div.parentNode;
 
-            let resetHref = resetCardPlano?.querySelector('a');
+        }
 
-            if(resetHref != null){
+        if (div.classList.contains('selected')) {
 
-              resetHref.href = urlPlanoPremiumAddApp;
 
-            }
 
-            if (cardPlano != null) {
-              let hrefLink = cardPlano.querySelector('a');
+          div.classList.remove('selected');
 
-              if (hrefLink != null) {
-                let index = hrefLink.href.indexOf('HD');
-                hrefLink.href =
-                  urlPlanoStandardPadrao.slice(0, index + 3) +
-                  newText +
-                  urlPlanoStandardPadrao.slice(index + 3);
+          let cardPlano = div.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 
-                let allSelectedsApps =
-                  this.elementRef.nativeElement.querySelectorAll(
-                    '.app img.selecionado'
-                  );
+          let url = "null";
 
-                if (allSelectedsApps != null) {
-                  allSelectedsApps.forEach((AppSelected: any) => {
-                    AppSelected.classList.remove('selecionado');
-                  });
-                }
+          if(cardPlano.id == 'standard' && div.id != null){
 
-                if (appSelecionado.classList.contains('selecionado')) {
-                  appSelecionado.classList.remove('selecionado');
-                } else {
-                  appSelecionado.classList.add('selecionado');
-                }
-              }
-            }
-          } else if (qualPlano.classList.contains('plano-premium')) {
-            let newText = '20%2B%20o%20aplicativo%20' + qualApp + '%';
+            url = "https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20300%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%20de%20domingo%20a%20domingo,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20em%20HD%20por%20apenas%20R%24%2064,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?";
 
-            let cardPlano = document.querySelector(
-              '.plano.plano-premium .card-plano'
-            );
 
-            let resetCardPlano = document.querySelector(
-              '.plano.plano-standard .card-plano'
-            );
+          }else if(cardPlano.id == 'premium' && div.id != null){
 
-            let resetHref = resetCardPlano?.querySelector('a');
+            url = "https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20500%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%20de%20domingo%20a%20domingo,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20em%20HD%20por%20apenas%20R%24%2084,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?";
 
-            if(resetHref != null){
-
-              resetHref.href = urlPlanoStandardAddApp;
-
-            }
-
-            if (cardPlano != null) {
-              let hrefLink = cardPlano.querySelector('a');
-
-              if (hrefLink != null) {
-                let index = hrefLink.href.indexOf('HD');
-                hrefLink.href =
-                  urlPlanoPremiumPadrao.slice(0, index + 3) +
-                  newText +
-                  urlPlanoPremiumPadrao.slice(index + 3);
-
-                console.log(hrefLink.href);
-
-                let allSelectedsApps =
-                  this.elementRef.nativeElement.querySelectorAll(
-                    '.app img.selecionado'
-                  );
-
-                if (allSelectedsApps != null) {
-                  allSelectedsApps.forEach((AppSelected: any) => {
-                    AppSelected.classList.remove('selecionado');
-                  });
-                }
-
-                if (appSelecionado.classList.contains('selecionado')) {
-                  appSelecionado.classList.remove('selecionado');
-                } else {
-                  appSelecionado.classList.add('selecionado');
-                }
-              }
-            }
           }
-        });
-      });
-    }
+
+          let hrefLink = cardPlano.querySelector('a');
+
+          hrefLink.href = url;
+
+
+        } else {
+
+          const choicesAppsSelected = this.elementRef.nativeElement.querySelectorAll('.app-choice.selected');
+
+          if (choicesAppsSelected != null) {
+
+            choicesAppsSelected.forEach((choiceSelected: any) => {
+
+              choiceSelected.classList.remove('selected');
+
+            })
+
+          }
+
+          div.classList.add('selected');
+
+          let cardPlano = div.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+
+          let url = "null";
+
+          if(cardPlano.id == 'standard' && div.id != null){
+
+            url = "https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20300%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%20de%20domingo%20a%20domingo,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20em%20HD%20por%20apenas%20R%24%2064,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?";
+
+
+          }else if(cardPlano.id == 'premium' && div.id != null){
+
+            url = "https://api.whatsapp.com/send?phone=558005914866&text=Ol%C3%A1,%0AGostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20plano%20de%20internet%20de%20500%20MEGA%20com%20instala%C3%A7%C3%A3o%20gr%C3%A1tis,%20suporte%20de%20domingo%20a%20domingo,%20Wi-Fi%205G%20de%20alta%20performance%20e%20mais%20de%20100%20canais%20de%20TV%20em%20HD%20por%20apenas%20R%24%2084,90/m%C3%AAs.%20Como%20fa%C3%A7o%20para%20adquirir?";
+
+          }
+
+          let hrefLink = cardPlano.querySelector('a');
+          let index = hrefLink.href.indexOf('HD')
+
+
+          let newText  = "20%2B%20o%20aplicativo%20" + div.id + "%";
+
+          if(div.id != null){
+
+            if (index !== -1) {
+              hrefLink.href = url.slice(0, index + 3) + newText + url.slice(index + 3);
+            }
+
+          }
+
+
+
+
+
+        }
+
+
+      })
+    })
+
   }
+
+
+
+
+
 }
